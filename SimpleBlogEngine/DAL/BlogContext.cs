e9875b7c7317628a543a106f7de6be4c20413792
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2018-10-16 00:11:12 +0200

    Added Data Access Layer

---
+++ SimpleBlogEngine/DAL/BlogContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using SimpleBlogEngine.Models;
+using DbContext = System.Data.Entity.DbContext;
+
+namespace SimpleBlogEngine.DAL
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext() : base("BlogContext")
+        {
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<User> Users { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Post> Posts { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Category> Categories { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<PostCategory> PostCategories { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<PostAuthor> PostAuthors { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
