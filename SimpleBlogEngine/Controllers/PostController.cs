134834b00421f8fc929fe67b328d2b7a0d5d6b4f
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2018-10-23 16:15:17 +0200

    Implemented deletion of posts

---
+++ SimpleBlogEngine/Controllers/PostController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleBlogEngine.DAL;
+using SimpleBlogEngine.Models;
+
+namespace SimpleBlogEngine.Controllers
+{
+    public class PostController : Controller
+    {
+        private BlogContext _context;
+
+        public PostController(BlogContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Remove(int id)
+        {
+            var post = _context.Posts.Where(p => p.PostID == id);
+            _context.Posts.Remove(post.First());
+            _context.SaveChanges();
+            return new RedirectResult("/");
+        }
+    }
+}
